3a93ac968dc2df860de7f81c4bdb3d67cd085f0a
oliealex <alexander_racho@hotmail.com>
2018-01-29 01:07:24 +0100

    Removed timer
    Might add it back later

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ import lxml
 import requests
 #import autopy
 import numpy
-import datetime
 import pyautogui
 #-----------------------
 from PIL import Image
@@ -22,22 +21,21 @@ Assuming you are playing on a 24' screen
 x_pad = 0
 y_pad = 96
 
-character_pos = (637, 460)
-
 char_xbox = 637 - 200
 char_ybox = 460 + 400)
 
 Play area = x_pad+1, y_pad +1, 1258, 797
 """
+page = requests.get('http://hordes.io/#')
+tree = html.fromstring(page.content)
+
 box_full = (x_pad + 1, y_pad + 1, x_pad + 1258, y_pad + 701)
 box_Hp = (11, 129, 206, 147)
 
 cord_hpBox = (109, 11)
 low_Hp = (87,109,96,255)
 
-now = datetime.datetime.now()
-laterSec = now.second + 20
-laterMin = now.minute + 15
+
 
 def new_timer():
     now = datetime.datetime.now()
@@ -60,19 +58,6 @@ def alertBox():
     if alert.alert("Do you want the bot to start/continue press OK", cancel_button="cancel") == False:
         bool = False
     return bool
-
-"""
-Not finished
-def timer():
-    now = datetime.datetime.now()
-    if now.minute == laterMin:
-        new_timer()
-        if alertBox() == False:
-            exit()
-    elif now.second == laterSec:
-        new_timer()
-        pyautogui.press('2')
-"""
 
 def attack():
     pyautogui.press('tab')
